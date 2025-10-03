3a425f1d5953a2ece2c50391f3c3a5143f29ad7a
Mozul504 <mozul.ai@phystech.edu>
2025-10-03 16:54:10 +0300

    qwe

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-import subprocess
-import os
-
-# Переходим в нужную директорию
-project_path = r"C:\Users\Александр\Desktop\project"
-os.chdir(project_path)
-
-try:
-    # Инициализируем git репозиторий
-    result = subprocess.run(["git", "init"], check=True, capture_output=True, text=True)
-    print("Git репозиторий успешно создан!")
-    print(result.stdout)
-except subprocess.CalledProcessError as e:
-    print(f"Ошибка: {e}")
-    print(f"Stderr: {e.stderr}")
-except FileNotFoundError:
-    print("Git не установлен на системе!")
+print('hellllo')
